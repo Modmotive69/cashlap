594b0968cbfdab75c77395677d139081d8740a67
PenPen <penpen@smartdealer.com>
2026-06-09 18:06:26 -0400

    config: update Base44 app ID to Scott's own app (6a288841c1da28a78175ffab)

--- src/lib/app-params.js
+++ src/lib/app-params.js
@@ -36,7 +36,7 @@ const getAppParamValue = (paramName, { defaultValue = undefined, removeFromUrl =
 
 const getAppParams = () => {
 	return {
-		appId: getAppParamValue("app_id", { defaultValue: import.meta.env.VITE_BASE44_APP_ID }),
+		appId: getAppParamValue("app_id", { defaultValue: import.meta.env.VITE_BASE44_APP_ID || '6a288841c1da28a78175ffab' }),
 		serverUrl: getAppParamValue("server_url", { defaultValue: import.meta.env.VITE_BASE44_BACKEND_URL }),
 		token: getAppParamValue("access_token", { removeFromUrl: true }),
 		fromUrl: getAppParamValue("from_url", { defaultValue: window.location.href }),
